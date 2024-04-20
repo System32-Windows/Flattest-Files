80eb32a4ccf9922dc21d5b864aebc6c5ff0f3166
System32-Windows <pango.land@outlook.com>
2024-04-20 14:10:04 +0200

    FlattestScript

--- FlattestCDN/script.js
+++ FlattestCDN/script.js
@@ -1,34 +1,33 @@
 var att = 4;
-var wrong = false;
 var tablew = {
-    0: "QWxldGFuaW9fOA==",
-    1: "RGVtb25fUm9ibG94",
-    2: "YnVsZHVydA==",
-    3: "R29vZEd1ZXN0"
+    0: "Aletanio_8",
+    1: "Demon_Roblox",
+    2: "buldurt",
+    3: "GoodGuest"
 };
 
 var table = {
-    0: "Rk85MTU=",
-    1: "RkEwMDE=",
-    2: "RkEwMDI=",
-    3: "Z3Vlc3Q=",
+    0: "FO915",
+    1: "FA001",
+    2: "FA002",
+    3: "FV001",
 };
 
 function submit() {;
-    if (att <= 0) { document.getElementById("warn").innerHTML = `Wrong username. No attempts left.`; } else {;
-        for (i = 0; i < 4; i++) {;
-            if (atob(tablew[i]) === document.getElementById("who").value) {;
-                if (atob(table[i]) === document.getElementById("pass").value) {;
-
-                };
+    if (att <= 0) {
+        document.getElementById("warn").innerHTML = `Wrong username and/or password. No attempts left.`;
+    } else {;
+        if (tablew[0] === document.getElementById("who").value || tablew[1] === document.getElementById("who").value || tablew[2] === document.getElementById("who").value || tablew[3] === document.getElementById("who").value) {;
+            if (table[0] === document.getElementById("pass").value || table[1] === document.getElementById("pass").value || table[2] === document.getElementById("pass").value || table[3] === document.getElementById("pass").value) {;
+                document.getElementById("warn").innerHTML = ""
+                document.getElementById("thefiles").innerHTML = `<h3>The zip password is ${atob("RmxhdHRlc3RGaWxlcw==")}</h3><a href='Labels.zip'>Label Pack</a><br><a href='Posters%20and%20Infographics.zip'>Posters and Infographics Pack</a><br><a href='Logo%20Pack.zip'>Logo Pack</a><br><a href='Spam%20Pack.zip'>Spam Pack</a><br>`
             } else {
-                document.getElementById("warn").innerHTML = `Wrong username. You have ${att} attempts left.`;
-                wrong = true;
-            };
-        };
-        if (wrong) {;
-            att--;
-            wrong = false;
+                document.getElementById("warn").innerHTML = `Wrong FPID. You have ${att} attempts left.`;
+                att--
+            }
+        } else {
+            document.getElementById("warn").innerHTML = `Wrong username. You have ${att} attempts left.`;
+            att--
         };
     };
 };
